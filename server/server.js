0ed208072246fca2a7152a1f8740a92d3af67c2c
gohilpruthvirajsinh610-del <gohilpruthvirajsinh610@gmail.com>
2026-09-21 14:23:04 +0530

    Update CORS for Vercel

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: "https://gohilpruthvirajsinh610-del.github.io",
+    origin: "https://portfolio-seven-ashen-ii8hl87wjh.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
